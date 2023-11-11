53e27b169d23b58e546ad879475a5a364959a708
v2ging <kge3818@gmail.com>
2023-11-11 23:34:22 +0900

    [RoundN] 자바스크립트 기초 실

---
+++ .github/v2ging/Round1/프로그래밍과 데이터 in JavaScript/레시피만들기.js
@@ -0,0 +1,31 @@
+// 아래 코드중 잘못된 부분을 수정해 주세요
+function cloneObject(object) {
+  let temp = {};
+  for (let key in object) {
+    temp[key] = object[key];
+  }
+  return temp;
+}
+
+let espresso = ["espresso"];
+
+let americano = espresso.slice();
+americano.push("water");
+
+let caffeLatte = espresso.slice();
+caffeLatte.push("milk");
+
+// 여기에 caffeMocha와 vanillaLatte 레시피를 만들어 주세요
+
+let caffeMocha = caffeLatte.slice();
+caffeMocha.push("chocolateSyrup");
+
+let vanillaLatte = caffeLatte.slice();
+vanillaLatte.push("vanillaSyrup");
+
+// 테스트 코드
+console.log(espresso);
+console.log(americano);
+console.log(caffeLatte);
+console.log(caffeMocha);
+console.log(vanillaLatte);
