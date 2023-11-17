853bea0a549900ba7951a03da9e8e243f7c3e406
ccjngwn <ahaha620@gmail.com>
2023-11-18 00:21:30 +0900

    [Round2] promise 객체

--- Round2/자바스크립트 웹 개발 기본기/code.js
+++ Round2/자바스크립트 웹 개발 기본기/code.js
@@ -1,19 +1,18 @@
-// fetch('https://learn.codeit.kr/api/members/3')
-//   .then((response) => response.text())
-//   .then((result) => {
-//     console.log(result);
-//   });
+// Array Destructuring
+const members = ['코딩하는효준', '글쓰는유나', '편집하는민환'];
+const [macbook, ipad, coupon] = members;
 
-// const member = {
-//   name: 'Alice',
-//   email: 'alicey@codeitmall.kr',
-//   department: 'marketing',
-// };
+console.log(macbook); // 코딩하는효준
+console.log(ipad); // 글쓰는유나
+console.log(coupon); // 편집하는민환
 
-fetch('https://learn.codeit.kr/api/members/2', {
-  method: 'DELETE',
-})
-  .then((response) => response.text())
-  .then((result) => {
-    console.log(result);
-  });
+// Object Destructuring
+const macbookPro = {
+  title: '맥북 프로 16형',
+  price: 3690000,
+};
+
+const { wpahr, rkrur } = macbookPro;
+
+console.log(wpahr); // 맥북 프로 16형
+console.log(rkrur); // 3690000
