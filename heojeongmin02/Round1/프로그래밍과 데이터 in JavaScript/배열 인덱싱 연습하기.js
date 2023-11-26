d5abea9c93088ed27115eca275fe20618b5613ce
Eugene Kim <eugene02876@gmail.com>
2023-11-27 00:34:23 +0900

    fix: 외부 폴더 제거

--- heojeongmin02/Round1/프로그래밍과 데이터 in JavaScript/배열 인덱싱 연습하기.js
+++
@@ -1,6 +0,0 @@
-let dataType = ["number", "string", "boolean", "null", "undefined", "object"];
-
-// 여기에 코드를 작성하세요
-for (let index in dataType) {
-  console.log(dataType[index]);
-}
--- hhhkdev/Round1/2. 프로그래밍과 데이터 in JavaScript/2-2. 배열 인덱싱 연습하기.js
+++
@@ -1,9 +0,0 @@
-let dataType = ['number', 'string', 'boolean', 'null', 'undefined', 'object'];
-
-// 여기에 코드를 작성하세요
-console.log(dataType[0])
-console.log(dataType[1])
-console.log(dataType[2])
-console.log(dataType[3])
-console.log(dataType[4])
-console.log(dataType[5])
