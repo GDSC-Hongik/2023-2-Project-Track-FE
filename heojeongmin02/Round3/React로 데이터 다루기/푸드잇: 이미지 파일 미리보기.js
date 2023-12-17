721ee976da4151fbf1cd555e9c557d9a7e1adbe5
heojeongmin02 <1234@g.hongik.ac.kr>
2023-12-17 16:53:53 +0900

    [Round3] React로 데이터 다루기

---
+++ heojeongmin02/Round3/React로 데이터 다루기/푸드잇: 이미지 파일 미리보기.js
@@ -0,0 +1,52 @@
+import { useEffect, useRef, useState } from "react";
+
+function FileInput({ name, value, onChange }) {
+  const [preview, setPreview] = useState();
+  const inputRef = useRef();
+
+  const handleChange = (e) => {
+    const nextValue = e.target.files[0];
+    onChange(name, nextValue);
+  };
+
+  const handleClearClick = () => {
+    const inputNode = inputRef.current;
+    if (!inputNode) return;
+
+    inputNode.value = "";
+    onChange(name, null);
+  };
+
+  useEffect(() => {
+    if (!value) return;
+
+    const nextPreview = URL.createObjectURL(value);
+    setPreview(nextPreview);
+
+    return () => {
+      setPreview();
+      URL.revokeObjectURL(nextPreview);
+    };
+  }, [value]);
+  /**
+   *  아래 코드를 참고해서 미리보기 기능을 만들어주세요!
+   *
+   *  ObjectURL 만들기
+   *  const objectURL = URL.createObjectURL(file);
+   *
+   *  ObjectURL 해제하기
+   *  URL.revokeObjectURL(objectURL);
+   */
+
+  return (
+    <div>
+      <img src={preview} alt="이미지 미리보기" />
+      <input type="file" onChange={handleChange} ref={inputRef} />
+      <button type="button" onClick={handleClearClick}>
+        X
+      </button>
+    </div>
+  );
+}
+
+export default FileInput;
